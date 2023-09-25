ce90c7d4206181d8473e0b18d4c318020b537571
Andres Lopez <andres_lopez82161@elpoli.edu.co>
2023-09-25 09:04:14 -0500

    Fin de la seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
